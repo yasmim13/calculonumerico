f2a11e620bf845eb27d19bbfc97b500c2c225832
Yasmim Vitoria Correia De Sousa <yasmimvitoria10@gmail.com>
2019-05-18 21:20:59 -0300

    adicionando problema

--- gauss.c
+++ gauss.c
@@ -1,23 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define  L 4
+#define  C 5
 
-void formar_matriz(float m[3][4],float aux[3][4]);
-void imprimir_matriz(float m[3][4]);
+void formar_matriz(float m[L][C],float aux[L][C]);
+void imprimir_matriz(float m[L][C]);
 
 int main (void)
 {
     int i,j,k;
-    float m[3][4] ={{2,1,-3,-1},
-                  {-1,3,2,12},
-                  {3,1,-3,0}};
-   float mult, aux[3][4];
+    float m[L][C] ={{0.2345, 0.6793, 0.0145, 1.3315, 3.032},
+                  {2.2360, 1.9861, 1.7723, 2.0315, 2.135},
+                  {1.7723, -7.3324, 5.1961, -2.8284, 1.015},
+                  {2.4142, 1.7320, -5.3171, 5.6785, 2.101}};
+   float mult, aux[L][C];
 
-    for(k=0; k<2; k++)  /*laço dos passos*/
+    for(k=0; k<3; k++)  /*laço dos passos*/
     {
-        for(i=k+1; i<3; i++)  /*laço das linhas*/
+        for(i=k+1; i<L; i++)  /*laço das linhas*/
         {
             mult = m[i][k]/m[k][k];
-            for(j=0; j<4 ; j++)   /*laço das colunas*/
+            for(j=0; j<C ; j++)   /*laço das colunas*/
                 aux[i][j] = m[i][j]-(mult*m[k][j]);
         }
 
@@ -29,23 +32,23 @@ int main (void)
 
 
 /*coloca as linhas novas na matriz original*/
-void formar_matriz(float m[3][4],float aux[3][4])
+void formar_matriz(float m[L][C],float aux[L][C])
 {
-    for (int i=1;i<3;i++)
+    for (int i=1;i<L;i++)
     {
-        for(int j=0;j<4;j++)
+        for(int j=0;j<C;j++)
             m[i][j]=aux[i][j];
     }  
     return;
 }
 
-void imprimir_matriz(float m[3][4])
+void imprimir_matriz(float m[L][C])
 {
 
     printf("\n");
-    for(int i=0;i<3;i++)
+    for(int i=0;i<L;i++)
     {
-        for(int j=0;j<4;j++)
+        for(int j=0;j<C;j++)
             printf("%f ",m[i][j]);
         printf("\n");
     }
