ef8c30807034bc20f8e50bd28725b10520a717a7
Yasmim Vitoria Correia De Sousa <yasmimvitoria10@gmail.com>
2019-05-19 20:29:02 -0300

    fim

--- gauss.c
+++ gauss.c
@@ -9,9 +9,13 @@
 void formar_matriz(float m[L][C],float aux[L][C]);
 void imprimir_matriz(float m[L][C]);
 
+void iteracoes(float m[L][C],float i[4], int max);
+
 int main (void)
 {
     int i,j,k;
+    float s[4]={1.21,0.32,2.41,0.35};
+
     float m[L][C] ={{0.2345, 0.6793, 0.0145, 1.3315, 3.032},
                     {sqrt(5), 1.9861, -sqrt(PI), 2.0315, 2.135},
                     {sqrt(PI), -7.3324, 3*sqrt(3), -2*sqrt(2), 1.015},
@@ -30,10 +34,35 @@ int main (void)
 
         formar_matriz(m,aux);
         imprimir_matriz(m);
-    }       
+    }
+    
+        iteracoes(m,s,0);
     return 0;
 }
 
+void iteracoes(float m[L][C],float i[4], int max)
+{
+    float v[4];
+
+    if(max<10)
+    {
+
+    v[3] =i[3]+m[3][4]/ m[3][3];
+   v[2]= i[2]+(1/m[2][2])*(m[2][4]-(m[2][3]*v[3]));
+   v[1]=i[1]+(1/m[1][1])*(m[1][4]-((m[1][2]*v[2])+(m[1][3]*v[3])));
+   v[0]=i[0]+(1/m[0][0])*(m[0][4]-((m[0][1]*v[1])+(m[0][2]*v[2])+(m[0][3]*v[3])));
+
+   printf("\n S%d(",max);
+ for(int i=0;i<4;i++)
+     printf(" %f; ",v[i]);
+
+ printf(")\n");
+
+ iteracoes(m,v,max+1);
+    }
+
+ return;
+}
 
 /*coloca as linhas novas na matriz original*/
 void formar_matriz(float m[L][C],float aux[L][C])
